c90a6144ca1797c627e7b2b7e56a62269c8a3ab6
清风 <64196371+w15926@users.noreply.github.com>
2021-10-02 17:08:30 +0800

    提交源码

---
+++ src/main/java/com/my/my_interface/MyInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.my.my_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInterfaceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyInterfaceApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/my/my_interface/api.java
@@ -0,0 +1,4 @@
+package com.my.my_interface;
+
+public class api {
+}
